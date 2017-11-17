9eb4fadb69fed923917012c9c1bbe12f67035f90
pkrisz0 <p.krisz89@gmail.com>
2017-11-17 12:58:10 +0100

    filter success

--- src/main/java/com/greenfox/pkrisz0/exam/controller/MainController.java
+++ src/main/java/com/greenfox/pkrisz0/exam/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.greenfox.pkrisz0.exam.controller;
 
 import com.greenfox.pkrisz0.exam.repository.CarRepo;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,55 +11,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-    Boolean diplomat;
+    String saveparam = "";
 
     @Autowired
     CarRepo carRepo;
 
-//    @GetMapping({"","/"})
-//    public String main(Model model, @RequestParam(required = false) String search){
-//
-//        if (search != null && search.matches("^.*[^a-zA-Z0-9-].*$") && search.length() <= 7){
-//            model.addAttribute("error", "Sorry, the submitted license plate is not valid.");
-//        } else if (search != null){
-//            model.addAttribute("cars", carRepo.findAllByPlateIsLike("%" + search + "%"));
-//        } else {
-//            model.addAttribute("cars", carRepo.findAll());
-//        }
-//        return "main";
-//    }
-
     @GetMapping({"","/"})
-    public String mainTwo(Model model, @RequestParam(required = false) String search){
+    public String main(Model model, @RequestParam(required = false) String search){
 
-        if (search != null && search.matches("^.*[^a-zA-Z0-9-].*$") && search.length() <= 7 ){
+        if (search != null && search.matches("^.*[^a-zA-Z0-9-].*$") && search.length() <= 7){
             model.addAttribute("error", "Sorry, the submitted license plate is not valid.");
-        } else if (search != null && diplomat==null || search != null && diplomat==false){
+        } else if (search != null){
             model.addAttribute("cars", carRepo.findAllByPlateIsLike("%" + search + "%"));
-        } else if (search != null && diplomat==true) {
-            model.addAttribute("cars", carRepo.findAllByPlateStartingWithAndPlateContains("DT", search));
-            diplomat = false;
         } else {
             model.addAttribute("cars", carRepo.findAll());
         }
+        saveparam = search;
         return "main";
     }
 
-    @GetMapping("/diplomatT")
-    public String diplomatT(){
-        diplomat = true;
-        return "redirect:/";
-    }
-
-    @GetMapping("/diplomat")
-    public String diplomat(Model model, @RequestParam(required = false) String search){
-        model.addAttribute("cars", carRepo.findAllByPlateIsLikeAndPlateStartingWith("%" + search + "%", "DT"));
+    @GetMapping("/diplomat") //it ain't pretty
+    public String diplomat(Model model){
+        model.addAttribute("cars", carRepo.findAllByPlateIsLikeAndPlateStartingWith("%" + saveparam + "%", "DT"));
+        saveparam = "";
         return "main";
     }
 
     @GetMapping("/police")
     public String police(Model model){
-        model.addAttribute("cars", carRepo.findAllByPlateStartingWith("RB"));
+        model.addAttribute("cars", carRepo.findAllByPlateIsLikeAndPlateStartingWith("%" + saveparam + "%", "RB"));
+        saveparam = "";
         return "main";
     }
 
